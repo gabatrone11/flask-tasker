a14856b5d88d81f16b5d96a6638b8d8288fb98d8
gabatrone11 <gabatrone11@gmail.com>
2015-11-25 19:01:37 -0800

    11/25/2015 third commit

--- project/views.py
+++ project/views.py
@@ -3,9 +3,10 @@
 
 from flask.ext.sqlalchemy import SQLAlchemy
 from functools import wraps
-from forms import AddTaskForm
+from forms import AddTaskForm, RegisterForm, LoginForm
 from flask import Flask, flash, redirect, render_template, \
     request, session, url_for
+import datetime
 
 
 # config
@@ -14,7 +15,7 @@ app = Flask(__name__)
 app.config.from_object('_config')
 db = SQLAlchemy(app)
 
-from models import Task
+from models import Task, User
 
 
 
@@ -41,16 +42,36 @@ def logout():
 @app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
+    form = LoginForm(request.form)
     if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME'] \
-                or request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid Credentials. Please try again.'
-            return render_template('login.html', error=error)
+        if form.validate_on_submit():
+            user = User.query.filter_by(name=request.form['name']).first()
+            if user is not None and user.password == request.form['password']:
+                 session['logged_in'] = True
+                 session['user_id'] = user.id
+                 flash('Welcome!')
+                 return redirect(url_for('tasks'))
+            else:
+                error = 'Invalid username or password.'
         else:
-            session['logged_in'] = True
-            flash('Welcome!')
-            return redirect(url_for('tasks'))
-    return render_template('login.html')
+            error = 'Both fields are required.'
+    return render_template('login.html', form=form, error=error)
+
+@app.route('/register/', methods=['GET','POST'])
+def register():
+    error = None
+    form = RegisterForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            new_user = User(
+                form.name.data,
+                form.email.data,
+                form.password.data)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Thanks for registering. Please login.')
+            return redirect(url_for('login'))
+    return render_template('register.html', form=form, error=error)
 
 
 @app.route('/tasks/')
@@ -76,6 +97,8 @@ def new_task():
             form.name.data,
             form.due_date.data,
             form.priority.data,
+            datetime.datetime.utcnow(),
+            '1',
             '1')
         db.session.add(new_task)
         db.session.commit()
